987a83f1426fb303eaac2224c13785c55f313985
Destructor <a2023112702@estudiantes.upsa.edu.bo>
2024-11-14 09:19:34 -0400

    Inicio del proyecto

---
+++ src/main/java/com/seb/floreriaeleclipse/FloreriaElEclipseApplication.java
@@ -0,0 +1,13 @@
+package com.seb.floreriaeleclipse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FloreriaElEclipseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FloreriaElEclipseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/seb/floreriaeleclipse/FloreriaElEclipseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seb.floreriaeleclipse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FloreriaElEclipseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
